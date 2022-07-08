91795be7c6beec5e74d08f3447842eb10efe51f4
aditnag <iamaditnag034@gmail.com>
2022-07-08 23:40:58 +0530

    A shortcut: get_object_or_404()

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,9 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from polls.models import Question
-
-
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
 
 
 # def index(request):
@@ -20,8 +15,23 @@ def index(request):
 #     return HttpResponse(template.render(context, request))
 
 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+
+# def detail(request, question_id):
+#     return HttpResponse("You're looking at question %s." % question_id)
+
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exist")
+    # return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
